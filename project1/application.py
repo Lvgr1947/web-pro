989ee2760b0e8bf88679580ac64c19a8a57fdd9b
Lvgr1947 <l.venugopalrao1947@gmail.com>
2020-06-03 19:29:42 +0530

    chnages made

--- project1/application.py
+++ project1/application.py
@@ -7,6 +7,7 @@ from flask import Flask, render_template, request, session, redirect, url_for, j
 from flask_session import Session
 from sqlalchemy import create_engine, or_
 from sqlalchemy.orm import scoped_session, sessionmaker
+from bookpage import getbook
 # from flask_marshmallow import Marshmallow
 # from flask import Flask, request, jsonify
 import models
@@ -81,89 +82,110 @@ def search():
         return render_template("registration.html",headline="")
 
 
-@app.route("/bookpage", methods=["GET","POST"])
-def bookpage():
-    result = request.args.get('values')
-    # search = "%{}%".format(result)
-    search = BOOK.query.filter(BOOK.isbn.like(result))
-    isbn = []
-    bname = []
-    author = []
-    year = []
-    for row in search:
-        isbn.append(row.isbn)
-        bname.append(row.bname)
-        author.append(row.author)
-        year.append(row.year)
-    # query1 = RATING.query.filter_by(RATING.isbn=result)
-    query1=db.query(RATING).all()
-    print(result)
-    print(query1)
-    # print(query1[1].isbn)
-    isbn = []
-    # id=[]
-    username = []
-    rating = []
-    review = []
-    for row in query1:
-        if row.isbn==result:
-            # id.append(row.id)
-            isbn.append(row.isbn)
-            username.append(row.username)
-            rating.append(row.rating)
-            review.append(row.review)
-    return render_template("bookpage.html", isbn=isbn,bname=bname,author=author,year=year,rating=rating,review=review,username=username,length=len(isbn),length1=len(rating))
+# @app.route("/bookpage", methods=["GET","POST"])
+# def bookpage():
+    # result = request.args.get('values')
+    # # search = "%{}%".format(result)
+    # search = BOOK.query.filter(BOOK.isbn.like(result))
+    # isbn = []
+    # bname = []
+    # author = []
+    # year = []
+    # for row in search:
+    #     isbn.append(row.isbn)
+    #     bname.append(row.bname)
+    #     author.append(row.author)
+    #     year.append(row.year)
+    # # query1 = RATING.query.filter_by(RATING.isbn=result)
+    # query1=db.query(RATING).all()
+    # print(result)
+    # print(query1)
+    # # print(query1[1].isbn)
+    # isbn = []
+    # # id=[]
+    # username = []
+    # rating = []
+    # review = []
+    # for row in query1:
+    #     if row.isbn==result:
+    #         # id.append(row.id)
+    #         isbn.append(row.isbn)
+    #         username.append(row.username)
+    #         rating.append(row.rating)
+    #         review.append(row.review)
+    # return render_template("bookpage.html", isbn=isbn,bname=bname,author=author,year=year,rating=rating,review=review,username=username,length=len(isbn),length1=len(rating))
 
+@app.route("/api/bookpage",methods = ["POST"])
+def apibookpage():
+    isbn = request.form.get("isbn")
+    bookreturned = getbook(isbn) 
+    Bookdetails = {}
+    Bookdetails["isbn"] = bookreturned[0].isbn
+    Bookdetails["title"] = bookreturned[0].title
+    Bookdetails["author"] = bookreturned[0].author
+    Bookdetails["year"] = bookreturned[0].year
+    return jsonify({"bookinfo":Bookdetails})
 
-@app.route("/rating", methods=["GET","POST"])
-def rating():     
+@app.route("/api/review",methods = ["POST"])
+def apibookpagereview():
+   username = session["username"]
+   isbn = request.form.get("isbn")
+   query = db.query(RATING).filter_by(isbn=isbn).first()
+   tot_reviews = {}
+   for i in query:
+       tot_reviews[i.username] = [i.isbn,i.review,i.rating] 
+   print(tot_reviews)
+   return jsonify({'total_review': tot_reviews})
+
+# @app.route("/rating", methods=["GET","POST"])
+# def rating():     
+    # # review = request.form['message']
+    # # rating = request.form['choice']
+    # username = session["username"]
+    # result = request.args.get('values')
+    # # return review
+    # query = db.query(RATING).filter_by(isbn=result, username=username).first()
+    # session["isbn"]=result
+    # # query = RATING.query.filter(RATING.isbn.like(f'%{result}%'),RATING.username.like(f'%{session["username"]}%'))
+    # if query is not None:
+    #     select = RATING.query.filter(RATING.isbn.like(f'%{result}%'),RATING.username.like(f'%{session["username"]}%'))
+    #     isbn = []
+    #     username = []
+    #     rating = []
+    #     review = []
+    #     for row in select:
+    #         isbn.append(row.isbn)
+    #         username.append(row.username)
+    #         rating.append(row.rating)
+    #         review.append(row.review)
+    #     return render_template("review.html", isbn=isbn,username=username,rating=rating,review=review,length=len(isbn))
+    # else:
+    #     query = BOOK.query.filter(BOOK.isbn.like(f'%{result}%'))
+    #     isbn = []
+    #     bname = []
+    #     author = []
+    #     year = []
+    #     for row in query:
+    #         isbn.append(row.isbn)
+    #         bname.append(row.bname)
+    #         author.append(row.author)
+    #         year.append(row.year)
+    #     return render_template("rating.html", isbn=isbn,bname=bname,author=author,year=year,length=len(isbn))
+
+        
+        
+# @app.route("/review", methods=["GET","POST"])
+# def review():
     # review = request.form['message']
     # rating = request.form['choice']
-    username = session["username"]
-    result = request.args.get('values')
-    # return review
-    query = db.query(RATING).filter_by(isbn=result, username=username).first()
-    session["isbn"]=result
-    # query = RATING.query.filter(RATING.isbn.like(f'%{result}%'),RATING.username.like(f'%{session["username"]}%'))
-    if query is not None:
-        select = RATING.query.filter(RATING.isbn.like(f'%{result}%'),RATING.username.like(f'%{session["username"]}%'))
-        isbn = []
-        username = []
-        rating = []
-        review = []
-        for row in select:
-            isbn.append(row.isbn)
-            username.append(row.username)
-            rating.append(row.rating)
-            review.append(row.review)
-        return render_template("review.html", isbn=isbn,username=username,rating=rating,review=review,length=len(isbn))
-    else:
-        query = BOOK.query.filter(BOOK.isbn.like(f'%{result}%'))
-        isbn = []
-        bname = []
-        author = []
-        year = []
-        for row in query:
-            isbn.append(row.isbn)
-            bname.append(row.bname)
-            author.append(row.author)
-            year.append(row.year)
-        return render_template("rating.html", isbn=isbn,bname=bname,author=author,year=year,length=len(isbn))
-
-        
-        
-@app.route("/review", methods=["GET","POST"])
-def review():
-    review = request.form['message']
-    rating = request.form['choice']
-    username = session["username"]
-    result = request.args.get('values')
-    # return result      
-    info = RATING(isbn=result,username=username,rating=rating,review=review)
-    db.add(info)
-    db.commit()
-    headline="Successfully added your review"
-    return render_template("search.html", headline = headline)
+    # username = session["username"]
+    # result = request.args.get('values')
+    # # return result      
+    # info = RATING(isbn=result,username=username,rating=rating,review=review)
+    # db.add(info)
+    # db.commit()
+    # headline="Successfully added your review"
+    # return render_template("search.html", headline = headline)
         
 
 @app.route("/auth", methods=["GET","POST"])
